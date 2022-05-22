875b5b6af1b234e5549153aefa7eaf233f97ff4d
Сергей <nechaevserega90@gmail.com>
2022-05-22 19:08:08 +0300

    lesson2

---
+++ main.py
@@ -0,0 +1,27 @@
+my_int = (1)
+print(my_int)
+print(type(my_int))
+
+my_str = "привет"
+print(my_str)
+print(type(my_str))
+
+a = 100
+b = 12.5
+c = a + b
+print(a + b)
+print(type(a + b))
+
+a = bool(10)
+print(a)
+print(type(a))
+
+
+def has_no_return():
+    pass
+
+
+has_no_return()
+
+print(type(has_no_return()))
+
